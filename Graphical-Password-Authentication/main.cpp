9963e6a07cadc8bfd0d0a765809030ac6e175022
Travis <travishill62102@gmail.com>
2022-09-16 23:57:06 -0400

    Added create button function. Tested if it created one manually in main

--- Graphical-Password-Authentication/main.cpp
+++ Graphical-Password-Authentication/main.cpp
@@ -1,11 +1,35 @@
 #include "mainwindow.h"
 
+#include <list>
+#include <iostream>
+
 #include <QApplication>
+#include <QPushButton>
+#include <QPixmap>
+#include <QIcon>
+#include <QString>
+
+QPushButton* createButton(QString filePath) {
+    QPushButton *button = new QPushButton("");
+    QPixmap picture(filePath);
+    QIcon icon(picture);
+    button->setIcon(icon);
+    QSize standardSize(100,100);
+    button->setIconSize(standardSize);
+    return button;
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    std::list<QString> testList;
+    testList.push_front("C:\\Users\\Owner\\Desktop\\Memes\\Dame.jpg");
+    QPushButton *firstTestButton = createButton(testList.front());
+    firstTestButton->show();
     w.show();
     return a.exec();
 }
+
+
